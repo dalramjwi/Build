54489c1dc8606693639cb54dbf567b9321e670b7
dalramjwi <dalramjwi@gmail.com>
2024-07-01 10:46:08 +0900

    install ts

--- work-1.js
+++ work-1.js
@@ -1,5 +1,9 @@
 function a(b) {
-  let result = b + 10;
-  return result;
+  if (typeof b === "number") {
+    if (Number.isInteger(b) === true) {
+      let result = b + 10;
+      return result;
+    }
+  }
 }
 console.log(a("string"));
